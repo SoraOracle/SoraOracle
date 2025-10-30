3a9267eaf5cfc6ccd2ff8a1651409d0ac28d3cfd
Sora <sora@soraoracle.io>
2025-10-30 03:41:01 +0000

    Created a checkpoint

--- scan/frontend/.next/server/next-font-manifest.js
+++ scan/frontend/.next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/runner/workspace/scan/frontend/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/db234bd00cda6a96-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
